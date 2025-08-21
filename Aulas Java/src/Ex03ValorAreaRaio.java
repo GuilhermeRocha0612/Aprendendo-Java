8f2a71edda48f8d9581b1e393741fe97420f7a29
Guilherme Rocha <guior06@hotmail.com>
2025-08-21 12:07:29 -0300

    Calculando Valor de uma área pelo raio
    Signed-off-by: Guilherme Rocha <guior06@hotmail.com>

---
+++ Aulas Java/src/Ex03ValorAreaRaio.java
@@ -0,0 +1,19 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Ex03ValorAreaRaio {
+    public static void main (String[] args ) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("---------Calculadora de área de um raio--------");
+        System.out.println("Digite o valor do raio: ");
+        double valorRaio = sc.nextDouble();
+        double pi = 3.14159;
+        double area = pi * (Math.pow(valorRaio, 2.0));
+
+        System.out.println("-----RESULTADO-----");
+        System.out.printf("O valor da área é igual a: %.4f%n", area);
+
+    }
+}
