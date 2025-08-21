9ee837aa1134db82669c8c7d7f80a2dd827ab748
Guilherme Rocha <guior06@hotmail.com>
2025-08-21 12:17:02 -0300

    Calculando a Diferença entre os produtos em Java
    Signed-off-by: Guilherme Rocha <guior06@hotmail.com>

--- Aulas Java/src/Ex04DiferencaProduto.java
+++ Aulas Java/src/Ex04DiferencaProduto.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Ex04DiferençaProduto {
+public class Ex04DiferencaProduto {
     public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
 
