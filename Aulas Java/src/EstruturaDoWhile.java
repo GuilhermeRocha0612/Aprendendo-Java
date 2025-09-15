803b10d1da30d3b3f923b604ed6207771d7948f6
Guilherme Rocha <guior06@hotmail.com>
2025-09-15 15:08:53 -0300

    Estrutura DoWhile

--- Aulas Java/src/EstruturaDoWhile.java
+++ Aulas Java/src/EstruturaDoWhile.java
@@ -5,12 +5,16 @@ public class EstruturaDoWhile {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+        char resposta;
 
-        System.out.println("Digite a temperatura em Celsius: ");
-        
-        double C = sc.nextDouble();
-        double F = 9.0 * C / 5.0 + 32.0;
-        System.out.printf("Equivalente em Fahrenheit: %.1f%n", F);
+        do {
+            System.out.println("Digite o valor da temperatura em Celsius: ");
+            double temperaturaC = sc.nextDouble();
+            double temperaturaF = 9.0 * temperaturaC / 5.0 + 32.0;
+            System.out.printf("Valor em Fahrenheit: %.1f%n", temperaturaF);
+            System.out.println("Deseja continuar? (s/n)");
+            resposta = sc.next().charAt(0);
+        } while (resposta != 'n');
 
         sc.close();
     }
