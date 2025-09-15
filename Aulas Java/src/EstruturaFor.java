813ba014e4196c2f9278e8c90892755d4aecbaed
Guilherme Rocha <guior06@hotmail.com>
2025-09-15 14:41:41 -0300

    Estruturas de repetição

---
+++ Aulas Java/src/EstruturaFor.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class EstruturaFor {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // int N = sc.nextInt();
+
+        // int soma = 0;
+
+        // for(int i=0; i<N; i++){
+        //     int x = sc.nextInt();
+        //     soma += x;
+        // }
+
+        // System.out.println(soma);
+
+        for (int i = 5; i>=0; i--) {
+            System.out.println("Valor de i: " + i);
+        }
+
+        sc.close();
+
+    }
+}
--- Aulas Java/src/EstruturaWhile.java
+++ Aulas Java/src/EstruturaWhile.java
@@ -1,14 +1,38 @@
-import java.util.Scanner;
 
 public class EstruturaWhile {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Digite um valor: (0 para parar)");
-        int x = sc.nextInt();
-        while(x != 0) {
-            System.out.println("Digite outro número: ");
-            x = sc.nextInt();
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Digite um valor: (0 para parar)");
+        // int x = sc.nextInt();
+
+        // int soma = 0;
+        // while(x != 0) {
+        //     soma += x;
+        //     System.out.println("Digite outro número: ");
+        //     x = sc.nextInt();
+        // }
+
+        // System.out.println("O valor da soma de todos os números digitados foi de: " + soma);
+        // sc.close(); 
+
+        // int x = 2;
+        // int y = 10;
+        // while(x < y){ 
+        //     System.out.println(x + "-" + y);
+        //     x =  x * 2;
+        //     y = y + 1;
+        // }
+
+        // System.out.println("FIM");
+
+        int x = 4;
+        int y = 0;
+        int i = 0;
+        while(i < x) {
+            i += 1;
+            y += i;
+            System.out.println(i);
+            System.out.println(y);
         }
-        sc.close();
     }
 }
