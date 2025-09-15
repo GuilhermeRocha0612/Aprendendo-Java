73049ae23e8ed4a7b25f20d3f6cd7ad3e61d26c1
Guilherme Rocha <guior06@hotmail.com>
2025-09-15 18:00:50 -0300

    Funções para Strings

---
+++ Aulas Java/src/FuncoesString.java
@@ -0,0 +1,15 @@
+public class FuncoesString {
+    public static void main(String[] args) {
+        
+        String original = "abdce FGHIJ ABC abc DEFG";
+
+        String s01 = original.toLowerCase();
+
+        String s02 = original.toUpperCase();
+
+        String s03 = original.trim();
+
+        System.out.println(s03);
+
+    }
+}
